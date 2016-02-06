baa6ae27c61a8078dc345a7cf3b22ef20cbebaa6
Lucas Godoy <2lucasg@gmail.com>
2016-02-06 16:50:18 -0300

    Little refactor of model fields validators.

--- src/area/models.py
+++ src/area/models.py
@@ -10,15 +10,14 @@ from jsonfield import JSONField
 class Provider(models.Model):
     name = models.CharField(max_length=256)
     email = models.EmailField()
-    phone_validator = RegexValidator(
-        regex=r'^\+?1?\d{9,15}$',
-        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
-    )
-    phone = models.CharField(max_length=15, validators=[phone_validator])
-    language = models.CharField(max_length=5, choices=settings.LANGUAGES)
-    # http://www.xe.com/iso4217.php
+    language = models.CharField(max_length=7, choices=settings.LANGUAGES)
+
+    phone = models.CharField(max_length=15, validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                             message="Phone number format: '+999999999'. Up to 15 digits allowed.")])
+
+    # ISO 4217 Currency Code (shttp://www.xe.com/iso4217.php)
     currency = models.CharField(max_length=3, validators=[RegexValidator(regex='^.{3}$',
-                                message='Length has to be 3', code='nomatch')])
+                                message='Length has to be 3')])
 
 
 class Polygon(models.Model):
